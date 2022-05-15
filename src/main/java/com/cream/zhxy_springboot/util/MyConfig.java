072e72b28da08ea38ad0b4c56d6e0c8d98b3e493
Fire-Cream <1738678946@qq.com>
2022-05-15 22:27:07 +0800

    智慧校园1.0 项目仅有基本配置，无任何功能

---
+++ src/main/java/com/cream/zhxy_springboot/util/MyConfig.java
@@ -0,0 +1,23 @@
+package com.cream.zhxy_springboot.util;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.cream.zhxy_springboot.mapper")
+public class MyConfig {
+
+    /**
+     * 分页插件
+     *
+     * @Return com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor interceptor = new PaginationInterceptor();
+        // paginationInterceptor.setLimit(你的最大单页限制数量，默认 500 条，小于 0 如 -1 不受限制);
+        return interceptor;
+    }
+}
---
+++ src/test/java/com/cream/zhxy_springboot/ZhxySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cream.zhxy_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhxySpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
